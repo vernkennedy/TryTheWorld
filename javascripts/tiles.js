a762e790460471ed22e22b5e92485b2c3cdc24d6
vernkennedy <vzidana14@wooster.edu>
2015-09-09 23:45:44 -0700

    first

--- javascripts/tiles.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('myTiles', []);
-app.controller('tilesCtrl', function($scope, $http) {
-  $http.get("https://raw.githubusercontent.com/vernkennedy/TryTheWorld/gh-pages/javascripts/tiles.json")
-  .success(function (response) {$scope.names = response.records;});
-});
